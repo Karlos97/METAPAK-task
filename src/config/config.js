11ad6359eb1b7d4df7eb2b78b73cb905aae6ba42
ads <asd>
2021-11-10 10:00:30 -0800

    deleting unnecessary files

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export const delayForNextFetechBookList = 5000;
-export const delayForNotificationDismiss = 1000;
-export const booksPerFetch = 15;
-export const bookListUrl =
-  'https://www.googleapis.com/books/v1/volumes?q=subject:';
-export const bookDetailsUrl =
-  'https://api.github.com/users/';
-export const appNamePathUsedInRouting = '/Metapack-github-users-app';
-export const bookDetailsPathUsedInRouting = '/user/:userId';
--- src/store/actions/notificationActions.js
+++
@@ -1,8 +0,0 @@
-import { SET_NOTIFICATION } from './actionsType';
-
-export const setNotification = (payload) => ({
-  type: SET_NOTIFICATION,
-  payload,
-});
-
-export default setNotification;
